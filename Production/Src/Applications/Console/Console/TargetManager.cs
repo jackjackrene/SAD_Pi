c9a5b36f7fa74ec10e2949b823c6b6358b3eacdd
thomasericdean <thomasericdean@gmail.com>
2015-02-26 19:49:41 -0800

    TargetManagerUpdate

--- Production/Src/Applications/Console/Console/TargetManager.cs
+++ Production/Src/Applications/Console/Console/TargetManager.cs
@@ -8,6 +8,23 @@ namespace Console
 {
     public class TargetManager
     {
+        private static TargetManager instance;
+        public static TargetManager Default
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new TargetManager();
+                }
+
+                return instance;
+            }
+
+        }
+
+
+
         IEnumerable<Target> TargetList;
         void Prioritize();
         private bool IsFriend(Target target);
