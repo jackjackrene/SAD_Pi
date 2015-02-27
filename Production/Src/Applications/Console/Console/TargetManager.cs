cb88166cad111993103ef906c29a163b804d2dbf
thomasericdean <thomasericdean@gmail.com>
2015-02-26 19:42:30 -0800

    Initial Classes

---
+++ Production/Src/Applications/Console/Console/TargetManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console
+{
+    public class TargetManager
+    {
+        IEnumerable<Target> TargetList;
+        void Prioritize();
+        private bool IsFriend(Target target);
+        public IEnumerable<Target> GetEnemies
+        {
+
+
+
+
+        }
+        public IEnumerable<Target> GetFriends
+        {
+
+
+
+        }
+
+
+        
+    }
+}
