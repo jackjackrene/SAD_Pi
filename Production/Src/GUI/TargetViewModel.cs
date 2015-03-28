0064b6be533baf2bb6dd106b8910fb082464e7ec
thomasericdean <thomasericdean@gmail.com>
2015-03-28 16:36:36 -0700

    Commit to merge branches

---
+++ Production/Src/GUI/TargetViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using GUI;
+using SAD.Core;
+
+
+
+namespace SAD.Core
+{
+    public class TargetViewModel : ViewModelBase
+    {
+        private Target m_target;
+
+        public TargetViewModel(Target target)
+        {
+            m_target = target;
+            KillTargetCommand = new MyCommand(KillTarget);
+        }
+
+        /// <summary>
+        /// Gets the target model data.
+        /// </summary>
+        public Target Target
+        {
+            get { return m_target; }
+        }
+
+        private void KillTarget()
+        {
+            //Do some stuff....
+
+           // m_target.IsAlive = false;
+        }
+
+        public ICommand KillTargetCommand { get; set; }
+    }
+}
---
+++ Production/Src/GUI/ViewModel/MyCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace SAD.Core
+{
+    public class MyCommand : ICommand
+    {
+        private Action m_action;
+
+        public MyCommand(Action someAction)
+        {
+            m_action = someAction;
+        }
+
+        #region Implementation of ICommand
+        /// <summary>
+        /// Defines the method that determines whether the command can execute in its current state.
+        /// </summary>
+        /// <returns>
+        /// true if this command can be executed; otherwise, false.
+        /// </returns>
+        /// <param name="parameter">Data used by the command.  If the command does not require data to be passed, this object can be set to null.</param>
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        /// <summary>
+        /// Defines the method to be called when the command is invoked.
+        /// </summary>
+        /// <param name="parameter">Data used by the command.  If the command does not require data to be passed, this object can be set to null.</param>
+        public void Execute(object parameter)
+        {
+            m_action();
+        }
+        public event EventHandler CanExecuteChanged;
+        #endregion
+    }
+}
