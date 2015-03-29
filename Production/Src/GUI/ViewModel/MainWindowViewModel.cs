d0ccd87d26e60407f41147fce7a631e2282f0834
thomasericdean <thomasericdean@gmail.com>
2015-03-29 16:42:04 -0700

    Folder Structure

---
+++ Production/Src/GUI/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace GUI
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+        private string m_title;
+
+        public MainWindowViewModel()
+        {
+            Title = "SAD.3.14 Controls";
+
+
+        }
+        /// <summary>
+        /// Setting the title for the window
+        /// </summary>
+        public string Title
+        {
+            get { return m_title; }
+            set
+            {
+                m_title = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+    }
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+      //  [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
