e1e4d06134bb0143246c5a1b78a0048eb53c1d30
thomasericdean <thomasericdean@gmail.com>
2015-03-31 16:56:53 -0700

    Revert "Trying to fix stuff"
    This reverts commit 8340c3fc45323cc8d0d93a53926a55340e0c2ccf.

--- Production/Src/GUI/View/MissileSelectorView.xaml.cs
+++ Production/Src/GUI/View/MissileSelectorView.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace GUI
+namespace GUI.View
 {
     /// <summary>
     /// Interaction logic for MissileSelectorView.xaml
