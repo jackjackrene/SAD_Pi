e9f51e9b4b44139ce0ab10935dd099491aa50980
thomasericdean <thomasericdean@gmail.com>
2015-03-31 16:58:05 -0700

    Revert "Revert "Trying to fix stuff""
    This reverts commit e1e4d06134bb0143246c5a1b78a0048eb53c1d30.

--- Production/Src/GUI/ViewModel/MainWindowViewModel.cs
+++ Production/Src/GUI/ViewModel/MainWindowViewModel.cs
@@ -30,4 +30,21 @@ namespace GUI.ViewModel
             }
         }
     }
+
+    /* Moved to its own seperate file
+     * 
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    //    [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+    */ 
 }
