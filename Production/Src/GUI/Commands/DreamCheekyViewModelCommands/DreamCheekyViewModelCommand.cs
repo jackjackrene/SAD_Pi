f96ef61306fa746becf28e57c67a0692cfe2006f
jadmr <@sz6xU0NM1ia1tEqs6RWv>
2015-03-31 17:19:24 -0700

    Added a command for for the DreamCheeky ViewModel

---
+++ Production/Src/GUI/Commands/DreamCheekyViewModelCommands/DreamCheekyViewModelCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace GUI.Commands.DreamCheekyViewModelCommands
+{
+    class DreamCheekyViewModelCommand : ICommand
+    {
+        // Fields
+        private Action DreamCheekyViewModelAction;
+        public event EventHandler CanExecuteChanged; // I think this is for CanExecute()
+
+        // Constructor
+        public DreamCheekyViewModelCommand(Action action)
+        {
+            DreamCheekyViewModelAction = action;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            // Some kind of check should go here
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            DreamCheekyViewModelAction();
+        }
+    }
+}
